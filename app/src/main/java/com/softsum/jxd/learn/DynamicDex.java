d8ca9d9af6b080e06f1ee78f14613cb33efa0667
jxdqwer <768688093@qq.com>
2018-09-06 09:31:09 +0800

    mmmm

---
+++ app/src/main/java/com/softsum/jxd/learn/DynamicDex.java
@@ -0,0 +1,13 @@
+package com.softsum.jxd.learn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DynamicDex extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dynamic_dex);
+    }
+}
---
+++ app/src/main/java/com/softsum/jxd/learn/dynamic/DynamicTestIn.java
@@ -0,0 +1,11 @@
+package com.softsum.jxd.learn.dynamic;
+
+/**
+ * @author jxd
+ * @date 2018/9/5-16:01
+ * @blog www.softsum.cn
+ */
+
+public interface DynamicTestIn {
+     String DynamicDexTestCode();
+}
